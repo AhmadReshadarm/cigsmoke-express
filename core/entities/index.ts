68662339df3e49262b0579bd3b5031f1ec00cdcc
Petr Stankin <stanoke@mail.ru>
2022-06-16 23:50:57 +0300

    added images-microservice

--- core/entities/index.ts
+++ core/entities/index.ts
@@ -3,4 +3,5 @@ export { User } from "./user.entity";
 export { Product } from './product.entity';
 export { Category } from './category.entity';
 export { Color } from './color.entity';
-export { Brand } from './brand.entity'
+export { Brand } from './brand.entity';
+export { Image } from './image.entity';
