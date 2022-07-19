a5bfde9da2ca41a897db0a8aa87bf6b48f73715a
Petr Stankin <stanoke@mail.ru>
2022-07-19 18:29:08 +0300

    added banners micro-service

--- core/entities/index.ts
+++ core/entities/index.ts
@@ -1,6 +1,7 @@
 export { User } from "./users/user.entity";
 export { Image } from './images/image.entity';
 export { Review } from './reviews/review.entity';
+export { Slide, Advertisement } from './banners/';
 export { Wishlist } from './wishlists/wishlist.entity';
 export { Basket, OrderProduct, PaymentCard, Address, Checkout } from './orders'
 export { Brand, Category, Color, Product, Parameter, Tag } from './catalog'
