fbd3a3f740b5b983d3227259df6d3bb364fd56c4
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2022-08-06 20:57:24 +0300

    Fixed reviews dockerfile

--- core/lib/dto.ts
+++ core/lib/dto.ts
@@ -1,15 +1,13 @@
-import { ProductDTO, UserDTO } from '../../reviews/reviews.dtos';
-
 export interface PaginationDTO<I> {
-  rows: I[],
-  length: number
+  rows: I[];
+  length: number;
 }
 
 export interface RatingDTO {
-  '1'?: number,
-  '2'?: number,
-  '3'?: number,
-  '4'?: number,
-  '5'?: number,
-  avg: number,
+  '1'?: number;
+  '2'?: number;
+  '3'?: number;
+  '4'?: number;
+  '5'?: number;
+  'avg': number;
 }
